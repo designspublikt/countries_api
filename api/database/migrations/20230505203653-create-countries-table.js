627b2738c9c45c251caad61decbb105670c8c1c7
designspublikt <designspublikt@gmail.com>
2023-05-05 18:18:23 -0300

    All Done

--- api/database/migrations/20230505203653-create-countries-table.js
+++ api/database/migrations/20230505203653-create-countries-table.js
@@ -1,22 +1,20 @@
-'use strict';
-
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Countries", {
+      CountryId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      CountryName: Sequelize.STRING,
+      CountryPopulation: Sequelize.STRING,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Countries");
+  },
 };
